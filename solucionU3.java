93f81b190604dd724aad2a92bac706a87e0b04d5
yahir <brandomyahirsantos@gmail.com>
2026-03-25 08:43:52 -0600

    commit2

--- solucionU3.java
+++ solucionU3.java
@@ -4,18 +4,42 @@ public class solucionU3 {
 public static void main(String[] args); 
 Scanner scan = new Scaner.nextline(System.in);
 
+int opcion 
 int faturas = 0;
 int iva = .16;
 
-do {
-System.out.print("ingresar las facturas del dia de hoy:" );
-nextline();
-System.out.print("impuesto de iva: ");
-nextline();
-while (i=<i++) }
+    do opcion {
+        System.out.print("ingresar las facturas del dia de hoy:" );
+        nextline(System.in);
+        System.out.print("impuesto de iva: ");
+        nextline(System.in);
+        System.out.print(cerrar turno);
+        nextline(System.in);
+    while (i=<1++) }
 
 
-kjdf
+    for (i<1; i=0;i++){
+    system.out.print("numero de facturas:");
+    system 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
